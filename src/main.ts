f028b36bad1da52393751d9dfb0fe17fcc153256
EuJanderGois <jender.man33@gmail.com>
2024-12-20 00:09:27 -0300

    Adicionada pagina de em manutenção

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,13 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+  <div class="center">
+    <img src="./maintenance.svg">
+    <h1>Estamos em Manutenção</h1>
+    <p>Desculpe pelo inconveniente, mas nosso site está passando por manutenção.</p>
+    <p>Estamos trabalhando duro para melhorar sua experiência.</p>
+    <p>Por favor, volte mais tarde.</p>
   </div>
 `
 
